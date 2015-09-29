efb295df09c5ce43bcd1d8244c54a5b61c06cbbf
Kasper Peulen <kasperpeulen@gmail.com>
2015-09-29 19:39:58 +0200

    camelcase

--- {{cookiecutter.component_name | replace("-","_")}}/{{cookiecutter.component_name | replace("-","_")}}.dart
+++ {{cookiecutter.component_name | replace("-","_")}}/{{cookiecutter.component_name | replace("-","_")}}.dart
@@ -4,4 +4,4 @@ import 'package:angular2/angular2.dart';
 
 @Component(selector: '{{cookiecutter.component_name }}')
 @View(templateUrl: '{{cookiecutter.component_name | replace("-","_")}}.html', directives: const [])
-class {{cookiecutter.component_name | capitalize | replace('-','')}} {}
+class {{cookiecutter.component_name | title | replace('-','')}} {}
